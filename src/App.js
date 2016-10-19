780d2a092bd11cbdc02d11033f5b0e946455f8cc
Moti Zilberman <motiz88@gmail.com>
2016-10-19 17:48:59 +0300

    Fix styles and add checking/fixing scripts
    This uses `eslint-config-babel` for consistency with other Babel projects, which could come in handy down the line.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 // @flow
 
-import React, { Component } from 'react';
-import SelectableList from './SelectableList'
-import Repl from './Repl'
-import persistence from './persistence'
-import mergeOptions from './mergeOptions'
-import { PRESETS, PLUGINS, PRESET_NAMES, CONTAINING_PRESETS } from './data'
-import _ from 'lodash'
-import './App.css'
+import React, { Component } from "react";
+import SelectableList from "./SelectableList";
+import Repl from "./Repl";
+import persistence from "./persistence";
+import mergeOptions from "./mergeOptions";
+import { PRESETS, PLUGINS, PRESET_NAMES, CONTAINING_PRESETS } from "./data";
+import _ from "lodash";
+import "./App.css";
 
 const INIT = `class X {
 	foo: string = 'bar'
-}`
+}`;
 
 class App extends Component {
-	state = {
-		presets: {},
-		plugins: {},
-		code: INIT,
-		options: {},
-	}
+  state = {
+    presets: {},
+    plugins: {},
+    code: INIT,
+    options: {},
+  }
 
-	render() {
-		const presets = this.selectedPresets()
-		const plugins = this.selectedPlugins()
-		const { options } = this.state
+  render() {
+    const presets = this.selectedPresets();
+    const plugins = this.selectedPlugins();
+    const { options } = this.state;
 
-		return (
+    return (
 			<div className="repl">
 				{this.state.showConfig && (
 					<dialog open={true} className="dialog">
@@ -42,8 +42,8 @@ class App extends Component {
 						items={PRESET_NAMES}
 						selected={this.state.presets}
 						disabled={presets.reduce((carry, curr) => {
-							return [ ...carry, ...(CONTAINING_PRESETS[curr] || []) ]
-						}, [])}
+  return [ ...carry, ...(CONTAINING_PRESETS[curr] || []) ];
+}, [])}
 						onItemToggle={this.togglePreset}
 						options={options}
 						onOptionToggle={this.togglePresetOption}
@@ -55,8 +55,8 @@ class App extends Component {
 						items={PLUGINS}
 						selected={this.state.plugins}
 						disabled={presets.reduce((carry, curr) => {
-							return [ ...carry, ...PRESETS[curr] ]
-						}, [])}
+  return [ ...carry, ...PRESETS[curr] ];
+}, [])}
 						onItemToggle={this.togglePlugin}
 						options={options}
 					/>
@@ -67,102 +67,102 @@ class App extends Component {
 					presets={presets}
 					plugins={plugins}
 					options={options}
-					onChange={code => this.setState({ code })}
+					onChange={(code) => this.setState({ code })}
 				/>
 
 			</div>
-		)
-	}
+		);
+  }
 
-	selectedPresets() {
-		return this.selected('presets')
-	}
+  selectedPresets() {
+    return this.selected("presets");
+  }
 
-	selectedPlugins() {
-		return this.selected('plugins')
-	}
+  selectedPlugins() {
+    return this.selected("plugins");
+  }
 
-	selected(key) {
-		return Object.keys(this.state[key]).filter(k => this.state[key][k])
-	}
+  selected(key) {
+    return Object.keys(this.state[key]).filter((k) => this.state[key][k]);
+  }
 
-	xcomponentWillMount() {
-		this.setState(persistence.load())
-	}
+  xcomponentWillMount() {
+    this.setState(persistence.load());
+  }
 
-	xcomponentDidUpdate() {
-		persistence.save({
-			code: this.state.code,
-			presets: this.presets,
-			babili: false, // TODO
-			lineWrap: false, // TODO
-			evaluate: false, // TODO
-		})
-	}
+  xcomponentDidUpdate() {
+    persistence.save({
+      code: this.state.code,
+      presets: this.presets,
+      babili: false, // TODO
+      lineWrap: false, // TODO
+      evaluate: false, // TODO
+    });
+  }
 
-	updatePresetOption = (preset, changes) => {
-		this.setState({
-			options: {
-				...this.state.options,
-				[preset]: {
-					...this.state.options[preset],
-					...changes,
-				}
-			}
-		})
-	}
+  updatePresetOption = (preset, changes) => {
+    this.setState({
+      options: {
+        ...this.state.options,
+        [preset]: {
+          ...this.state.options[preset],
+          ...changes,
+        }
+      }
+    });
+  }
 
-	togglePresetOption = (preset) => {
-		this.updatePresetOption(preset, {
-			enabled: this.state.options[preset] ? !this.state.options[preset].enabled : true,
-		})
-	}
+  togglePresetOption = (preset) => {
+    this.updatePresetOption(preset, {
+      enabled: this.state.options[preset] ? !this.state.options[preset].enabled : true,
+    });
+  }
 
-	handlePresetOptionChanged = (preset, opts) => {
-		this.updatePresetOption(preset, {
-			value: opts,
-		})
-	}
+  handlePresetOptionChanged = (preset, opts) => {
+    this.updatePresetOption(preset, {
+      value: opts,
+    });
+  }
 
-	togglePreset = preset => {
-		this.setState(state => ({
-			presets: _.omit({
-				...state.presets,
-				[preset]: !state.presets[preset],
-			}, CONTAINING_PRESETS[preset]),
-			plugins: _.omit(state.plugins, PRESETS[preset]),
-		}))
-	}
+  togglePreset = (preset) => {
+    this.setState((state) => ({
+      presets: _.omit({
+        ...state.presets,
+        [preset]: !state.presets[preset],
+      }, CONTAINING_PRESETS[preset]),
+      plugins: _.omit(state.plugins, PRESETS[preset]),
+    }));
+  }
 
-	togglePlugin = plugin => {
-		this.setState(state => ({
-			plugins: {
-				...state.plugins,
-				[plugin]: !state.plugins[plugin],
-			},
-		}))
-	}
+  togglePlugin = (plugin) => {
+    this.setState((state) => ({
+      plugins: {
+        ...state.plugins,
+        [plugin]: !state.plugins[plugin],
+      },
+    }));
+  }
 
-	showConfig = () => this.setState({ showConfig: true })
-	hideConfig = () => this.setState({ showConfig: false })
+  showConfig = () => this.setState({ showConfig: true })
+  hideConfig = () => this.setState({ showConfig: false })
 
-	generateConfig() {
-		const config = {}
+  generateConfig() {
+    const config = {};
 
-		const presets = Object.keys(this.state.presets).filter(k => this.state.presets[k])
-		const plugins = Object.keys(this.state.plugins).filter(k => this.state.plugins[k])
-		const { options } = this.state
+    const presets = Object.keys(this.state.presets).filter((k) => this.state.presets[k]);
+    const plugins = Object.keys(this.state.plugins).filter((k) => this.state.plugins[k]);
+    const { options } = this.state;
 
-		if (presets.length) {
-			config.presets = mergeOptions(presets, options)
-		}
+    if (presets.length) {
+      config.presets = mergeOptions(presets, options);
+    }
 
-		if (plugins.length) {
-			config.plugins = mergeOptions(plugins, options)
-		}
+    if (plugins.length) {
+      config.plugins = mergeOptions(plugins, options);
+    }
 
-		return JSON.stringify(config, null, 2)
-	}
+    return JSON.stringify(config, null, 2);
+  }
 }
 
-export default App
+export default App;
