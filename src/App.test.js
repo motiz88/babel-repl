b06fe165ef9ace1ac8dedbeb55e36d1266f82544
Moti Zilberman <moti@digital-detox.co.uk>
2017-03-19 22:05:38 +0000

    test(App): Add Redux store to test

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import configureStore from "./state/configureStore";
+import { Provider } from "react-redux";
 
 describe("App", () => {
-  let div;
+  let div, store;
 
   beforeEach(() => {
     div = document.createElement("div");
     document.body.appendChild(div);
+    store = configureStore();
   });
 
   it("renders without crashing", () => {
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      div
+    );
   });
 
   afterEach(() => {
